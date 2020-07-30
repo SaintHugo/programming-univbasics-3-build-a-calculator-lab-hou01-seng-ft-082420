a6a32c4e2cf968cb11c5990bedf86828dba8a416
Hugo Rodrigez <hugor37@gmail.com>
2020-07-30 21:54:32 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,25 @@
 def addition(num1, num2)
-sum = num1 + num2
-puts sum
-return sum
+result = num1 + num2
+puts result
+return result
 end
 
 def subtraction(num1, num2)
-  sum = num1 - num2
-  puts sum
-  return sum
+  result = num1 - num2
+  puts result
+  return result
 end
 
 def division(num1, num2)
-  sum = num1 / num2
-  puts sum
-  return sum
+  result = num1 / num2
+  puts result
+  return result
 end
 
 def multiplication(num1, num2)
-
+  result = num1 * num2
+  puts result
+  return result
 end
 
 def modulo(num1, num2)
